546299bf18cf49f35d71759a5ab46abc38044f47
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-07-27 13:21:10 +0530

    Blogs Display is working now
    Hit the Blogs Data api from frontend and got the data, sent it in <Blogs> as props. Works just fine for now.

--- src/App.js
+++ src/App.js
@@ -4,33 +4,42 @@ import Home from "./Pages/Home.js";
 import Navibar from "./Navibar.js";
 import Create from "./Pages/Create.js";
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogPost from "./Pages/BlogPost.js";
 
 function App() {
-    const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState([]);
 
-    const handleBlog = (inp) => {
-        setBlogs((prevBlogs) => [...prevBlogs, inp]);
-    };
-    return (
-        <div>
-            <Navibar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/blog" element={<Blog blogs={blogs} />} />
-                <Route path="/about" element={<About />} />
-                <Route
-                    path="/Create"
-                    element={<Create handleBlog={handleBlog} />}
-                />
-                <Route
-                    path="/blog/:id"
-                    element={<BlogPost blogs={blogs} />}
-                />
-            </Routes>
-        </div>
-    );
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/blogsData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    response = await response.json();
+    setBlogs(response);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const handleBlog = (inp) => {
+    setBlogs((prevBlogs) => [...prevBlogs, inp]);
+  };
+  return (
+    <div>
+      <Navibar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog blogs={blogs} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Create" element={<Create handleBlog={handleBlog} />} />
+        <Route path="/blog/:id" element={<BlogPost blogs={blogs} />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/Navibar.js
+++ src/Navibar.js
@@ -1,35 +1,37 @@
 import logo from "./head.png";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Navibar = () => {
   return (
     <div>
       <nav className="Navbar">
         <div className="title">
-         <Link to="/">
-          <img src={logo} alt="chris's head cartoon"></img>
-        </Link>
-        <Link to="/">
-          <h1>shaan</h1>
-        </Link>
+          <Link to="/">
+            <img src={logo} alt="chris's head cartoon"></img>
+          </Link>
+          <Link to="/">
+            <h1>shaan</h1>
+          </Link>
         </div>
         <ul className="otherLinks">
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link to="/blog">Blogs</Link>
           </li>
           <li>
-          <Link to="https://www.linkedin.com/in/abhinavchdhary/">Linkedin</Link>
+            <Link to="https://www.linkedin.com/in/abhinavchdhary/">
+              Linkedin
+            </Link>
           </li>
           <li>
-          <Link to="/about">About</Link>
+            <Link to="/about">About</Link>
           </li>
           <li>
-          <Link to="/Create">Create</Link>
+            <Link to="/Create">Create</Link>
           </li>
         </ul>
       </nav>
     </div>
   );
-}
+};
 
 export default Navibar;
